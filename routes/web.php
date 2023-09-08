094bb2354f54c51a8744945704fe561014adeea2
Haikal-Adityo <haikaladityo@gmail.com>
2023-09-09 02:14:57 +0700

    Commit pertemuan 4 (pembuatan screenshot laprak)

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [TestController::class, 'beranda']);
 Route::get('/about', [TestController::class, 'about']);
 Route::get('/index', [TestController::class, 'index']);
