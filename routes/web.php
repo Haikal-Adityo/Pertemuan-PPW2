8d1d89f85f5a62698534890958b694112b7992fb
Haikal-Adityo <haikaladityo@gmail.com>
2023-09-12 14:40:59 +0700

    commit pertemuan 5 12/09/2023

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\BukuController;
 
 Route::get('/', [TestController::class, 'beranda']);
 Route::get('/about', [TestController::class, 'about']);
 Route::get('/index', [TestController::class, 'index']);
 
+//* PERTEMUAN 4
+
 Route::get('/boom', [TestController::class, 'boomesport']);
 Route::get('/prx', [TestController::class, 'prxesport']);
 Route::get('/fnatic', [TestController::class, 'fnaticesport']);
 Route::get('/fpx', [TestController::class, 'fpxesport']);
+
+
+//* PERTEMUAN 5
+
+// Route::get('/buku', 'BukuController@index');
+Route::get('/buku', [BukuController::class, 'index']);
