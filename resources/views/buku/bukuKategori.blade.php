7f962cfd2bdd4de6df1c80332cc174f090c36d75
Haikal-Adityo <haikaladityo@gmail.com>
2023-12-19 09:09:21 +0700

    UAS

--- resources/views/buku/bukuKategori.blade.php
+++ resources/views/buku/bukuKategori.blade.php
@@ -2,7 +2,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Daftar Buku {{ $kategori->name }}') }}
+            {{ __('Daftar Buku') }}
         </h2>
     </x-slot>
 
